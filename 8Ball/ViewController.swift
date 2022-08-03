3eddbb0df5dc5f2b7cee99fa8a89808adf680a2c
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-03 11:19:55 +0400

    Initial Commit

---
+++ 8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8Ball
+//
+//  Created by ლუკა ზუხბაია on 8/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
